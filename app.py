031179ca8e2b36508948d3e33b2788e7c4d7cbfe
8arb4ra <sisedubarbaraifb@gmail.com>
2026-09-03 13:35:08 +0000

    versão top

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template , request , redirect;
+from flask import Flask, render_template , request , redirect , session;
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave_secreta'
 
 @app.route("/")
 def login():
     return render_template ("login.html")
 
-
 @app.route("/painel")
 def painel():
-    return render_template ("painel.html")
+    if 'usuario_nome' in session:
+        return render_template("painel.html", usuario_nome=session['usuario_nome'], usuario_cpf=session['usuario_cpf'])
+    return redirect('/')
 
 @app.route("/verificar" , methods=['POST'])
 def verificar():
     CPF = request.form.get('CPF')
-     senha = request.form.get('senha')
+    senha = request.form.get('senha')
 
-print('Tentando login com:', CPF,'/SENHA',senha)
+    print('Tentando login com:', CPF, '/SENHA', senha)
+    
+    if CPF == '12345678900' and senha =='123456':
+        session['usuario_nome'] = "Barbara"
+        session['usuario_cpf'] = CPF
+        return redirect('/painel')
 
-  return redirect('/')
+    if CPF == '98765432100' and senha == '654321':
+        session['usuario_nome'] = "Nalo"
+        session['usuario_cpf'] = CPF
+        return redirect('/painel')
+
+    return redirect('/')
+@app.route("/sair")
+def sair():
+    session.pop('usuario_nome',None)
+    session.pop('usuario_cpf',None)
+    return redirect('/')
 
 if __name__ == "__main__":
    app.run(debug=True)
